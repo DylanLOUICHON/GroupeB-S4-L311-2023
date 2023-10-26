04e2c9f97f6bbdb22f8fff0d35b074427f9e7080
AmmarAlHusan <123240760+AmmarAlHusan@users.noreply.github.com>
2023-10-26 16:25:44 +0200

    fichier index.php modifié
    Correction de fichier

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
-<?php includ 'inc/inc.functions.php'; ?>
+<!-- Inclure le fichier des fonctions PHP -->
+<?php include_once('inc/inc.functions.php');?>
 <!DOCTYPE HTML>
-<!--
-	Story by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Story by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<?php include 'inc/inc.css.php'; ?>
+		<!-- Inclure le fichier PHP qui relie les fichiers CSS -->
+		<?php include ('inc/inc.css.php'); ?>
 	</head>
 	<body class="is-preload">
-
-		<!-- Wrapper -->
-			<div id="wrapper" class="divided">
-				<?php 
-					getPagesTemplate(
-						array_key_exist('page', $_GET) ? $_GET['page'] : null
-					); 
-				?>
-				<?php include 'inc/tpls-footer.php'; ?>
-			</div>
-
-		<?php includes 'inc/inc.js.php'; ?>
-
+		<!-- Contenu de la page -->
+		<div id="wrapper" class="divided">
+			<?php
+				// Appel à une fonction pour générer le contenu de la page
+				getPageTemplate(
+				array_key_exists('page', $_GET) ? $_GET['page'] : null
+				); 		
+			?>
+		</div>
+		<!-- Inclure le fichier PHP qui relie les fichiers JavaScript -->
+		<?php include ('inc/inc.js.php'); ?>
 	</body>
+	<?php
+		// Inclure le pied de page (footer)
+		include ('inc/tpl-footer.php');
+	?>
 </html>
