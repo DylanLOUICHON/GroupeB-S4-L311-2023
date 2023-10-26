e65919d6b830e3104b726eab763d0eeba0306989
AmmarAlHusan <123240760+AmmarAlHusan@users.noreply.github.com>
2023-10-26 16:51:44 +0200

    inc.functions.php
    le fichier inc.functions.php est corrigé et commenté

--- inc/inc.functions.php
+++ inc/inc.functions.php
@@ -1,67 +1,87 @@
+
 <?php
-    session_start();
+// Démarrage de la session
+session_start();
 
-    define('TL_ROOT', dirname(__DIR__));
-    define('LOGIN', 'UEL311');
-    define('PASSWORD', 'U31311');
-    define('DB_ARTICLES', TL_ROOT.'/dbal/articles.json');
+ // Définition des constantes
+define('TL_ROOT', dirname(__DIR__));
+define('LOGIN', 'UEL311');
+define('PASSWORD', 'UEL311');
+define('DB_ARTICLES', TL_ROOT.'/db/articles.json');
 
-    function connectUser($login = null, $password = null){
-        if(!is_null($login) && !is_null($password)){
-            if($login === LOGIN && $password === PASSWORD){
-                return array(
-                    'login'    => LOGIN,
-                    'password' => PASSWORD
-                );
-            }
+// Fonction de vérification du LOGIN et du PASSWORD
+function connectUser($login = null, $password = null)
+{
+    if (!is_null($login) && !is_null($password)) {
+        // Si l'identifiant et le mot de passe correspondent aux constantes définies
+        if ($login === LOGIN && $password === PASSWORD) {
+            return array(
+                'login'    => LOGIN,
+                'password' => PASSWORD
+            );
         }
-        return null;
     }
+    // Si l'identifiant et le mot de passe ne correspondent pas aux constantes définies
+    return null;
+}
 
-    function setDisconnectUser(){
-         unset($_SESSION['User']);
-         sessions_destroy();
+// Fonction qui déconnecte l'utilisateur
+function setDisconnectUser()
+{
+    // Suppression de la clé 'User' dans la session
+    unset($_SESSION['User']);
+    // Destruction de la session
+    session_destroy();
+}
+
+// Fonction de vérification pour déterminer si un utilisateur est connecté
+function isConnected()
+{
+    if (
+        // L'utilisateur est connecté
+        array_key_exists('User', $_SESSION)
+        && !is_null($_SESSION['User'])
+        && !empty($_SESSION['User'])
+    ) {
+        return true;
     }
-
-    function isConnected(){
-        if(array_key_exists('User', $_SESSION) 
-                && !is_null($_SESSION['User'])
-                    && !empty($_SESSION['User'])){
-            return true;
-        }
-        return false;
-    }
-
-    function getPageTemplate($page = null){
+    // L'utilisateur n'est pas connecté
+    return false;
+}
+//Une fonction qui récupère  les articles 
+  function getPageTemplate($page = null){
         $fichier = TL_ROOT.'/pages/'.(is_null($page) ? 'index.php' : $page.'.php');
 
         if(!file_exists($fichier)){
-            inclde TL_ROOT.'/pages/index.php';
+            include TL_ROOT.'/pages/index.php';
         }else{
             include $fichier;
         }
     }
-
-    function getArticlesFromJson(){
-        if(file_exist(DB_ARTICLE)) {
-            $contenu_json = file_get_contents(DB_ARTICLE);
-            return json_decode($contenu_json, true);
-        }
-
-        return null;
+//Une fonction qui permet de trouver tous les articles 
+function getArticlesFromJson()
+{
+    if (file_exists(DB_ARTICLES)) {
+        $contenu_json = file_get_contents(DB_ARTICLES);
+        return json_decode($contenu_json, true);
     }
+    //on retourne rien si la recherche échoue 
+    return null;
+}
 
-    function getArticleById($id_article == null){
-       if(file_exists(DB_ARTICLE)) {
-            $contenu_json = file_get_contents(DB_ARTICLE);
-            $_articles    = json_decode($contenu_json, true);
-
-            foreach($_articles as $article){
-                if($article['id'] == $id_article){
-                    return $article;
-                }
+//Une Fonction qui permet de chercher l'article par son identifiant 
+function getArticleById($id_article = null)
+{
+    if (file_exists(DB_ARTICLES)) {
+        $contenu_json = file_get_contents(DB_ARTICLES);
+        $_articles    = json_decode($contenu_json, true);
+        foreach ($_articles as $article) {
+            if ($article['id'] == $id_article) {
+                //la fonction retourne l'article désiré 
+                return $article;
             }
         }
-
-        return null;
     }
+    //La fonction return null si l'identifiant n'existe pas dans le fichier JSON 
+    return null;
+}
